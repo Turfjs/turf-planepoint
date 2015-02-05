aa63cc4d6cffef83727e45741768b5043c46e05a
Lyzi Diamond <lyzidiamond@gmail.com>
2015-02-04 16:41:05 -0800

    Switch example to geojson primitives

--- index.js
+++ index.js
@@ -9,20 +9,40 @@
  * @param {Polygon} triangle a Polygon feature with three vertices
  * @return {number} the z-value for `interpolatedPoint`
  * @example
+ * var point = {
+ *   "type": "Feature",
+ *   "properties": {},
+ *   "geometry": {
+ *     "type": "Point",
+ *     "coordinates": [-75.3221, 39.529]
+ *   }
+ * };
  * var point = turf.point([-75.3221, 39.529]);
  * // triangle is a polygon with "a", "b",
  * // and "c" values representing
  * // the values of the coordinates in order.
- * var triangle = turf.polygon([[
- *  [-75.1221, 39.57],
- *  [-75.58, 39.18],
- *  [-75.97, 39.86],
- *  [-75.1221, 39.57]
- * ]],
- *   {"a": 11, "b": 122, "c": 44});
+ * var triangle = {
+ *   "type": "Feature",
+ *   "properties": {
+ *     "a": 11,
+ *     "b": 122,
+ *     "c": 44
+ *   },
+ *   "geometry": {
+ *     "type": "Polygon",
+ *     "coordinates": [[
+ *       [-75.1221, 39.57],
+ *       [-75.58, 39.18],
+ *       [-75.97, 39.86],
+ *       [-75.1221, 39.57]
+ *     ]]
+ *   }
+ * };
  *
- * var features = turf.featurecollection(
- *  [triangle, point]);
+ * var features = {
+ *   "type": "FeatureCollection",
+ *   "features": [triangle, point]
+ * };
  *
  * var zValue = turf.planepoint(point, triangle);
  *
